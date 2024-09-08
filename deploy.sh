5d8c82bbbe7df01a66a7854a800b0268f737c71a
xvanausloos <xavier.vanausloos@ladonneeintelligente.com>
2024-09-08 12:08:40 +0200

    update deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+helm repo add apache-airflow https://airflow.apache.org
+helm upgrade --install airflow apache-airflow/airflow --namespace airflow --create-namespace
+
+kubectl port-forward svc/airflow-webserver 8080:8080 --namespace airflow
+
+Open Airflow:
+localhost:8080
+
+admin/admin
