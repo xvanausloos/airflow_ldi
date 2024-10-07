2411cd74171a0a290d9bd4bfbdb0f1c89c7c456d
xvanausloos <xavier.vanausloos@ladonneeintelligente.com>
2024-10-07 10:25:28 +0200

    use local DAG folder

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,5 @@
-#!/bin/bash
-helm repo add apache-airflow https://airflow.apache.org
+helm upgrade --install airflow apache-airflow/airflow -f override-values.yaml             
 
-export RELEASE_NAME=airflow-example-release
-export NAMESPACE=airflow-example-namespace
+kubectl port-forward svc/airflow-webserver 8080:8080 --namespace airflow-example-namespace
 
-kubectl delete namespace $NAMESPACE
-kubectl create namespace $NAMESPACE
-
-helm install $RELEASE_NAME apache-airflow/airflow \
-  --namespace $NAMESPACE \
-  --set-string "env[0].name=AIRFLOW__CORE__LOAD_EXAMPLES" \
-  --set-string "env[0].value=True"
-
-kubectl port-forward svc/$RELEASE_NAME-webserver 8080:8080 --namespace $NAMESPACE
 
