f5b591c73a27ce13ec2b9e96462459882da761fa
xvanausloos <xavier.vanausloos@ladonneeintelligente.com>
2024-10-09 06:20:44 +0200

    updage dag pg connect

--- dags/dag_pg_connect1.py
+++ dags/dag_pg_connect1.py
@@ -28,5 +28,6 @@ create_table_task = PostgresOperator(
             country TEXT NOT NULL,
             username TEXT NOT NULL,
             password TEXT NOT NULL,
-            email TEXT NOT NULL);'''   
+            email TEXT NOT NULL);''' ,
+    dag=dag
     )
