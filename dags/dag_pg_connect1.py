3aa0ccd005b5080c60070d702a075c43bfbc0f5d
xvanausloos <xavier.vanausloos@ladonneeintelligente.com>
2024-10-15 17:13:21 +0200

    feature: add _process_user xcom task

--- dags/dag_pg_connect1.py
+++ dags/dag_pg_connect1.py
@@ -23,7 +23,11 @@ def _process_user(ti):
     print(user)
     processed_user = pd.json_normalize({
         'firstname': user['name']['first'],
-        'lastname': user['name']['toto']
+        'lastname': user['name']['last'],
+        'country': user['location']['country'],
+        'username': user['login']['username'],
+        'password': user['login']['password'],
+        'email': user['email']
     })
     processed_user.to_csv('/tmp/processed_user.csv', index=None, header=False)
 
